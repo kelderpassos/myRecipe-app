b7de5d9541d3e07e62b2c8fe608afbb3328c0956
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-20 16:43:51 -0300

    adiciona await em response.json()

--- src/services/MealsAPI.js
+++ src/services/MealsAPI.js
@@ -3,48 +3,48 @@ const BASE_API = 'www.themealdb.com/api/json/v1/1';
 export const fetchMealById = async (id) => {
   const URL = `${BASE_API}/lookup.php?i=${id}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await await response.json();
   return data;
 };
 
 export const fetchMealByName = async (name) => {
   const URL = `${BASE_API}/search.php?s=${name}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 export const fetchMealsbyFirstLetter = async (letter) => {
   const URL = `${BASE_API}/search.php?f=${letter}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 export const fetchMealsByIngredient = async (ingredient) => {
   const URL = `${BASE_API}/filter.php?i=${ingredient}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 export const fetchMealsByCategory = async (category) => {
   const URL = `${BASE_API}/filter.php?c=${category}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 export const fetchMealsByArea = async (area) => {
   const URL = `${BASE_API}/filter.php?a=${area}`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 export const fetchRandomMeal = async () => {
   const URL = `${BASE_API}/random.php`;
   const response = await fetch(URL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
