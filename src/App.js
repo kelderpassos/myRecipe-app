d2bd41acfc07bddd07bb52dcdbeec65103f56a62
Kelder Passos <kelder.passos@gmail.com>
2022-06-20 19:32:59 -0300

    Fix: foram ajustados alguns detalhes de importação de ícones e posicionamento de atributos de tags

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './context/RecipesProvider';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <RecipesProvider>
       <BrowserRouter>
         <Header />
+        <Footer />
       </BrowserRouter>
     </RecipesProvider>
   );
