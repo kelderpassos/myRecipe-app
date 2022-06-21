1c631c008abdb707a626221fe121e5296ab08706
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-21 09:11:39 -0300

    corrigido req 5 e concluidos reqs 6, 7 e 8

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  saveMealsToken, saveCocktailsToken, saveUser, loadUser,
+} from '../services/StorageManager';
 
-function Login() {
+function Login({ history }) {
   const [input, setInput] = useState({
     email: '',
     password: '',
@@ -16,43 +20,57 @@ function Login() {
   const isLoginValid = () => {
     // Regex tirado do link https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
     const EMAIL_VALIDATION_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const PW_MINIMUM_LENGTH = 6;
+    const PW_MINIMUM_LENGTH = 7;
     const { email, password } = input;
     return EMAIL_VALIDATION_REGEX.test(email) && password.length >= PW_MINIMUM_LENGTH;
   };
 
-  const onUserLogin = () => {
-    console.log('login');
+  const onLoginSubmit = () => {
+    saveMealsToken(1);
+    saveCocktailsToken(1);
+    saveUser(input.email);
+    history.push('/foods');
   };
 
   return (
     <form>
-      <input
-        data-testid="email-input"
-        type="email"
-        name="email"
-        value={ input.email }
-        onChange={ handleInputChange }
-      />
+      <div>
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ input.email }
+          onChange={ handleInputChange }
+        />
+      </div>
 
-      <input
-        data-testid="password-input"
-        type="password"
-        name="password"
-        value={ input.password }
-        onChange={ handleInputChange }
-      />
-
-      <button
-        data-testid="login-submit-btn"
-        type="button"
-        disabled={ !isLoginValid() }
-        onClick={ onUserLogin }
-      >
-        Enter
-      </button>
+      <div>
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ input.password }
+          onChange={ handleInputChange }
+        />
+      </div>
+      <div>
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+          disabled={ !isLoginValid() }
+          onClick={ onLoginSubmit }
+        >
+          Enter
+        </button>
+      </div>
     </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
