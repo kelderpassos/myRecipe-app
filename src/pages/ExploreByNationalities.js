e8d94c06ec6a17c5ddd06fe3b2430c52ee8327f9
Kelder Passos <kelder.passos@gmail.com>
2022-06-22 19:41:16 -0300

    Feat: Footer completo

--- src/pages/ExploreByNationalities.js
+++ src/pages/ExploreByNationalities.js
@@ -1,5 +1,13 @@
-function ExploreByNationalities() {
+import React from 'react';
+import Footer from '../components/Footer';
 
+function ExploreByNationalities() {
+  return (
+    <div>
+      <h1>Nationalities</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default ExploreByNationalities;
--- src/pages/RecipeProgress.js
+++ src/pages/RecipeProgress.js
@@ -1,5 +1,11 @@
-function RecipeProgress() {
+import React from 'react';
 
+function RecipeProgress() {
+  return (
+    <div>
+      <h1>Progress</h1>
+    </div>
+  );
 }
 
 export default RecipeProgress;
