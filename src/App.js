be5c1eea919d387a3862bd22e578f31d582aa316
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-23 17:00:48 -0300

    alterações nas rotas e no css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import RecipesProvider from './context/RecipesProvider';
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';
-import RecipeDetails from './pages/RecipeDetails';
-import RecipeProgress from './pages/RecipeProgress';
+import RecipePage from './pages/RecipePage';
 import Explore from './pages/Explore';
 import ExploreCategories from './pages/ExploreCategories';
 import ExploreByIngredients from './pages/ExploreByIngredients';
@@ -22,10 +21,10 @@ function App() {
           <Route exact path="/" component={ Login } />
           <Route exact path="/foods" component={ MainPage } />
           <Route exact path="/drinks" component={ MainPage } />
-          <Route exact path="/foods/:foodId" component={ RecipeDetails } />
-          <Route exact path="/drinks/:drinkId" component={ RecipeDetails } />
-          <Route exact path="/foods/:foodId/in-progress" component={ RecipeProgress } />
-          <Route exact path="/drinks/:drinkId/in-progress" component={ RecipeProgress } />
+          <Route exact path="/foods/:foodId" component={ RecipePage } />
+          <Route exact path="/drinks/:drinkId" component={ RecipePage } />
+          <Route exact path="/foods/:foodId/in-progress" component={ RecipePage } />
+          <Route exact path="/drinks/:drinkId/in-progress" component={ RecipePage } />
           <Route exact path="/explore" component={ Explore } />
           <Route exact path="/explore/foods" component={ ExploreCategories } />
           <Route exact path="/explore/drinks" component={ ExploreCategories } />
