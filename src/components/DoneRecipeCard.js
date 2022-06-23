1ed7ca2d2aace780f276d51412d011a23bcec681
FabioMartins <fabbio1000@gmail.com>
2022-06-23 15:35:49 -0300

    Feat: Header criado.

--- src/components/DoneRecipeCard.js
+++ src/components/DoneRecipeCard.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import RecipesHeader from './RecipesHeader';
 
 function DoneRecipeCard() {
   return (
-    <h1>Componente Done</h1>
+    <div>
+      <RecipesHeader />
+      <main>
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src={ thumb }
+          alt="card thumb"
+        />
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          Texto Categoria
+        </p>
+        <p
+          data-testid={ `${index}-horizontal-name` }
+        >
+          Texto Nome
+        </p>
+        <p
+          data-testid={ `${index}-horizontal-done-date` }
+        >
+          Texto Data que foi feito
+        </p>
+        <button
+          data-testid={ `${index}-horizontal-share-btn` }
+          type="button"
+        >
+          Share
+        </button>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/RecipesHeader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function RecipesHeader() {
+  const path = history.location.pathname;
+  console.log(path);
+
+  return (
+    <header>
+      <h1>
+        Done Recipes
+      </h1>
+      <button
+        data-testid="filter-by-all-btn"
+        type="button"
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-food-btn"
+        type="button"
+      >
+        Food
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        type="button"
+      >
+        Drinks
+      </button>
+    </header>
+  );
+}
