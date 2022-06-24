86830867d1ba7b08cd70af3e8d29da0e4c69f3db
FabioMartins <fabbio1000@gmail.com>
2022-06-24 16:07:30 -0300

    Feat: Requisitos 83,84 e 85 feitos.

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import { loadUser } from '../services/StorageManager';
 
 function UserProfile() {
+  const storedUser = loadUser();
+  console.log(storedUser.email);
+
   return (
     <div>
       <h1>Profile</h1>
+      <p data-testid="profile-email">{`Usuário: ${storedUser.email}`}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
