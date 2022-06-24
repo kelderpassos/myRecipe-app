d3a7fd677d5f8e5103a360b978b7f9434d3cda2f
FabioMartins <fabbio1000@gmail.com>
2022-06-24 17:09:36 -0300

    Feat: Requisito 69 feito. Redirecionando para as páginas de explorar Foods e Drinks.

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Explore() {
+  const history = useHistory();
+
+  const onClickExploreFoods = () => {
+    history.push('/explore/foods');
+  };
+
+  const onClickExploreDrinks = () => {
+    history.push('/explore/drinks');
+  };
   return (
     <div>
       <Header />
       <button
         data-testid="explore-foods"
         type="button"
+        onClick={ onClickExploreFoods }
       >
         Explore Foods
       </button>
@@ -16,6 +27,7 @@ function Explore() {
       <button
         data-testid="explore-drinks"
         type="button"
+        onClick={ onClickExploreDrinks }
       >
         Explore Drinks
       </button>
