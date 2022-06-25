fe1bd5ddd777919f5c8a400255f112d1963ccd31
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-25 09:54:31 -0300

    feitos requisitos 75 e 76

--- src/pages/ExploreIngredients.js
+++ src/pages/ExploreIngredients.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import IngredientCard from '../components/IngredientCard';
+import { fetchDrinksIngredients } from '../services/CocktailsAPI';
+import { fetchMealsIngredients } from '../services/MealsAPI';
+import { trimArray } from '../services/Helpers';
+
+const INGREDIENTS_NUMBER = 12;
 
 function ExploreIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+  const history = useHistory();
+  const path = history.location.pathname;
+  const isFood = path.includes('foods');
+
+  useEffect(() => {
+    const fecthAPI = async () => {
+      const data = isFood
+        ? await fetchMealsIngredients()
+        : await fetchDrinksIngredients();
+
+      const trimmedData = trimArray(data, INGREDIENTS_NUMBER, path);
+      const ingredientsNames = trimmedData
+        .map((element) => element.strIngredient || element.strIngredient1);
+      setIngredients(ingredientsNames);
+    };
+
+    fecthAPI();
+  }, [isFood, path]);
+
   return (
     <div>
       <Header />
-      <h1>Ingredients</h1>
+      {ingredients.map((ingredient, index) => (
+        <IngredientCard
+          key={ `ingredient${index}` }
+          index={ index }
+          name={ ingredient }
+          isFood={ isFood }
+        />
+      )) }
       <Footer />
     </div>
   );
