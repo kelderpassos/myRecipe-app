b99ad75ac0951fde9d30ce556b7c73ef7bd9a5b3
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-25 10:49:25 -0300

    alterada tag base para button, pois precisa executar uma função ao ser clicado

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -2,21 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function IngredientCard(props) {
-  const { index, isFood, name } = props;
+  const { index, isFood, name, searchRecipes } = props;
 
   const getIngredientImage = () => (isFood
     ? `https://www.themealdb.com/images/ingredients/${name}-Small.png`
     : `https://www.thecocktaildb.com/images/ingredients/${name}-Small.png`);
 
   return (
-    <div data-testid={ `${index}-ingredient-card` }>
+    <button
+      type="button"
+      data-testid={ `${index}-ingredient-card` }
+      onClick={ () => searchRecipes(name) }
+    >
       <img
+        className="card-image"
         data-testid={ `${index}-card-img` }
         src={ getIngredientImage() }
         alt="ingredient thumb"
       />
       <p data-testid={ `${index}-card-name` }>{name}</p>
-    </div>
+    </button>
   );
 }
 
@@ -24,6 +29,7 @@ IngredientCard.propTypes = {
   index: PropTypes.number.isRequired,
   isFood: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
+  searchRecipes: PropTypes.func.isRequired,
 };
 
 export default IngredientCard;
