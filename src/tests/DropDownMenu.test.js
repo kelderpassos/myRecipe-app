1b114996176e7c8bd7520a6d27a68d8d071a71e8
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-29 17:36:16 -0300

    testes da pagina ExploreByNationalities

---
+++ src/tests/DropDownMenu.test.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// // import userEvent from '@testing-library/user-event';
+// import App from '../App';
+// import renderWithRouter from './Helpers/renderWithRouter';
+// // import mock from './Helpers/testData';
+
+// describe('Testa o Menu de Nacionalidades é renderizado corretamente.', () => {
+//   it('Testa se a página de nacionalidades é renderizada', () => {
+//     const { history } = renderWithRouter(<App />);
+//     history.push('/explore/foods/nationalities');
+//     const main = screen.getByTestId('nationalities-page');
+//     expect(main).toBeInTheDocumento();
+//   });
+// });
