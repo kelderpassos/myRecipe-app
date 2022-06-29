e6efdb5ab996312406d0a9479dedcfc412a97ffe
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-06-29 12:31:02 -0300

    adicionado objeto de dados para nacionalidades

--- src/tests/Helpers/testData.js
+++ src/tests/Helpers/testData.js
@@ -1,12 +1,12 @@
 import mealsRecipes from './mealsRecipesData';
 import cocktailsRecipes from './cocktailsRecipesData';
 
-const recipesResponse = {
+const recipesData = {
   meals: mealsRecipes,
   drinks: cocktailsRecipes,
 };
 
-const cateogiresReponse = {
+const categoriesData = {
   meals: [
     { strCategory: 'Beef' },
     { strCategory: 'Breakfast' },
@@ -23,4 +23,36 @@ const cateogiresReponse = {
   ],
 };
 
-module.exports = { recipesResponse, cateogiresReponse };
+const mealsAreasData = {
+  meals: [
+    { strArea: 'American' },
+    { strArea: 'British' },
+    { strArea: 'Canadian' },
+    { strArea: 'Chinese' },
+    { strArea: 'Croatian' },
+    { strArea: 'Dutch' },
+    { strArea: 'Egyptian' },
+    { strArea: 'French' },
+    { strArea: 'Greek' },
+    { strArea: 'Indian' },
+    { strArea: 'Irish' },
+    { strArea: 'Italian' },
+    { strArea: 'Jamaican' },
+    { strArea: 'Japanese' },
+    { strArea: 'Kenyan' },
+    { strArea: 'Malaysian' },
+    { strArea: 'Mexican' },
+    { strArea: 'Moroccan' },
+    { strArea: 'Polish' },
+    { strArea: 'Portuguese' },
+    { strArea: 'Russian' },
+    { strArea: 'Spanish' },
+    { strArea: 'Thai' },
+    { strArea: 'Tunisian' },
+    { strArea: 'Turkish' },
+    { strArea: 'Unknown' },
+    { strArea: 'Vietnamese' },
+  ],
+};
+
+module.exports = { recipesData, categoriesData, mealsAreasData };
