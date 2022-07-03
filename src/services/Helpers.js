76fb280f5e570beb8c1f0becec947c7f9756bd28
Kelder Passos <kelder.passos@gmail.com>
2022-07-03 18:38:07 -0300

    código atualizado

--- src/services/Helpers.js
+++ src/services/Helpers.js
@@ -44,3 +44,5 @@ export const trimArray = (data, size, path) => {
   if (path.includes('foods')) return data.meals.slice(0, size);
   return data.drinks.slice(0, size);
 };
+
+console.log('Keldão bonitão');
